53d3ef2aa06ff077a33b929bb967b5f1723dab6c
Emircan Erol <emircanerol54@gmail.com>
2021-07-09 14:18:43 +0300

    run python with URL

--- source/pass_link.php
+++ source/pass_link.php
@@ -3,7 +3,6 @@ $input = $_POST["name"];
 echo "input: $input</br>";
 
 $command = escapeshellcmd("python get_infos.py -a '".$input."' ");
-# escapeshellarg(string $input): string
 $output = shell_exec($command);
 echo $output;
 echo "</br>end";
