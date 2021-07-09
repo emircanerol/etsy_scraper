1b50d567d7c4c7dadd5946df4571f618fe7513c1
Emircan Erol <emircanerol54@gmail.com>
2021-07-09 10:14:27 +0300

    run python with URL

--- source/pass_link.php
+++ source/pass_link.php
@@ -1,7 +1,4 @@
 <?php
-# $fp = fopen('connect.txt', 'w') or die("<br>Unable to open file!");
-# fwrite($fp ,$_POST["name"]);
-# fclose($fp);
 $input = $_POST["name"];
 echo $input;
 $command = escapeshellcmd('python get_infos.py "-a" "$input"');
